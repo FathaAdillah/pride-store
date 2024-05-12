7d201a736b15328e99b223913a1d36e8a1aa480c
Fatha Adillah <fatha.adillah12@gmail.com>
2024-05-12 16:48:46 +0700

    Third commit

--- routes/web.php
+++ routes/web.php
@@ -15,17 +15,3 @@ Route::middleware(['auth:sanctum', config('jetstream.auth_session'),  'verified'
     })->name('home');
 });
 
-// Route::get('/home', [HomeController::class, 'index'])->name('home');
-
-
-// Route::middleware(['auth:sanctum', config('jetstream.auth_session'),  'verified'])->prefix('admin')->group(function () {
-//     Route::get('/home', function () {
-//         return view('admin.home');
-//     })->name('home');
-// });
-
-// Route::middleware(['auth:sanctum', config('jetstream.auth_session'),  'verified'])->prefix('user')->group(function () {
-//     Route::get('/dashboard', function () {
-//         return view('dashboard');
-//     })->name('home');
-// });
